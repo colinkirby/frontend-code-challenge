9d839ce782248cfd0a2c485877beedc57241d2db
colinkirby <ckirby1@macalester.edu>
2019-04-29 17:33:01 -0500

    Completed coding challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "./Styles/Tabs.css";
+import "./Styles/Modal.css";
+import deliveryData from './deliveries-sample.json';
+import Delivery from './Components/Delivery.js'
+import Modal from 'react-bootstrap/Modal'
+import ReactModal from 'react-modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      modalRestaurant: "Test",
+      mobile: false,
+    };
+
+  }
+
+  handleOpenModal = (name) => {
+    this.setState({ showModal: true, modalRestaurant: name});
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+
+  resize() {
+      this.setState({mobile: window.innerWidth <= 760});
+  }
+
+  render() {
+    const monday = deliveryData.dropoffs[0].deliveries;
+    const tuesday = deliveryData.dropoffs[1].deliveries;
+    const wednesday = deliveryData.dropoffs[2].deliveries;
+    const thursday = deliveryData.dropoffs[3].deliveries;
+    const friday = deliveryData.dropoffs[4].deliveries;
+
+    let dispStyle = "Desktop";
+
+    if(this.state.mobile) {
+      dispStyle = "Mobile"
+    }
+
+    return(
+
+      <div class = {dispStyle}>
+        <ReactModal
+           className = "Modal"
+           isOpen={this.state.showModal}
+           contentLabel="Minimal Modal Example"
+           onRequestClose={this.handleCloseModal}
+           shouldCloseOnOverlayClick={true}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <p> {this.state.modalRestaurant} </p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
+
+        <p> Delivery Schedule </p>
+        <Tabs>
+          <TabList>
+            <Tab>Mon</Tab>
+            <Tab>Tue</Tab>
+            <Tab>Wed</Tab>
+            <Tab>Thu</Tab>
+            <Tab>Fri</Tab>
+          </TabList>
+
+          <TabPanel>
+            {Object.keys(monday).map((value, index) =>
+              <div class = "Delivery">
+                <Delivery
+                  openModal={this.handleOpenModal}
+                  deliveryData = {monday[value]}
+                  isMobile = {this.state.mobile} />
+              </div>
+            )}
+          </TabPanel>
+
+          <TabPanel>
+            {Object.keys(tuesday).map((value, index) =>
+              <div class = "Delivery">
+                <Delivery
+                  openModal={this.handleOpenModal}
+                  deliveryData = {tuesday[value]}
+                  isMobile = {this.state.mobile} />
+              </div>
+            )}
+          </TabPanel>
+
+          <TabPanel>
+            {Object.keys(wednesday).map((value, index) =>
+              <div class = "Delivery">
+                <Delivery
+                  openModal={this.handleOpenModal}
+                  deliveryData = {wednesday[value]}
+                  isMobile = {this.state.mobile} />
+              </div>
+            )}
+          </TabPanel>
+
+          <TabPanel>
+            {Object.keys(thursday).map((value, index) =>
+              <div class = "Delivery">
+                <Delivery
+                  openModal={this.handleOpenModal}
+                  deliveryData = {thursday[value]}
+                  isMobile = {this.state.mobile} />
+              </div>
+            )}
+          </TabPanel>
+
+          <TabPanel>
+            {Object.keys(friday).map((value, index) =>
+              <div class = "Delivery">
+                <Delivery
+                  openModal={this.handleOpenModal}
+                  deliveryData = {friday[value]}
+                  isMobile = {this.state.mobile} />
+              </div>
+            )}
+          </TabPanel>
+        </Tabs>
+      </div>
+    )
+  }
 }
+
+const ModalStyle = {
+
+}
+
+
 
 export default App;
---
+++ src/Components/Delivery.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import '../Styles/Delivery.css'
+
+
+class Delivery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+
+  render() {
+    const data = this.props.deliveryData;
+    const isSoldOut = data.soldOut;
+    const isPastCutoff = data.isPastCutoff;
+    const {openModal} = this.props;
+    const isMobile = this.props.isMobile
+
+    let info;
+    let button;
+    let buttonText = "Select Restaurant";
+    let sellingOut = "";
+
+    if(isMobile) buttonText = "Order Status Here";
+    if(data.sellingOut) sellingOut = "Hurry! It's Selling Out!"
+
+    if(isPastCutoff) {
+      button = <Button variant="primary" size = "lg" disabled>Cutoff time past</Button>
+    } else {
+      button = <Button variant="warning" size = "sm" active onClick={(e) => openModal(data.restaurantName)}>{buttonText}</Button>
+    }
+
+    if (isSoldOut) {
+      info =  <p> Sold out </p>;
+    } else {
+      info =
+        (<div>
+          <p class = "time"> Order by: &emsp;&ensp;&nbsp; <span style = {{border: '1px solid black'}}> {data.cutoff} </span> </p>
+          <p class = "time"> Delivery Time: <span style = {{border: '1px solid black'}}> {data.dropoff} </span></p>
+          {button}
+          <p> {sellingOut} </p>
+        </div>
+      );
+    }
+
+    return(
+      <div class = "wrapper">
+        <div class = "box a">
+          <img style = {logo} src = {data.logoUrl}/>
+        </div>
+
+        <div class = "box b">
+          <p> <b>{data.restaurantName}</b> </p>
+          {info}
+        </div>
+        <br/>
+      </div>
+    )
+
+  }
+}
+
+const logo = {
+  width: "120px",
+  height: "120px"
+
+}
+
+export default Delivery;
